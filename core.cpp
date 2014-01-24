9ff5b03358c93a799a388ec682062615e13cb877
Reuben Castelino <projectdelphai@gmail.com>
2014-01-23 20:40:47 -0800

    rearranged json for pieces and started getting first move

--- core.cpp
+++ core.cpp
@@ -13,9 +13,16 @@ int main() {
   Json::Reader reader;
   bool parseSuccess = reader.parse(json_raw, root, false);
   cout << "Black's Turn" << endl;
-  string move;
-  cin >> move;
-  cout << move << endl;
-  cout << "Hello World!" << endl;
+  cout << "Original Position?: " << endl;
+  string original_position;
+  cin >> original_position;
+  cout << "New Position?: " << endl;
+  string new_position;
+  cin >> new_position;
+  if (root[new_position].empty()) {
+    cout << "Valid Move!" << endl;
+  } else {
+    cout << "Invalid Move, try again." << endl;
+  }
   return 0;
 }
