78fec8a0adae9b6f9a505b76426b7fb89c0105c2
Reuben Castelino <projectdelphai@gmail.com>
2014-02-25 20:19:17 -0800

    finished capturing pieces

--- core.cpp
+++ core.cpp
@@ -10,6 +10,10 @@ using namespace std;
 bool valid_move(string player, string original_position, string new_position);
 void query_next_move(string player);
 void make_move(string original_position, string new_position);
+vector<string> check_for_capture(string new_position);
+void remove_captured(vector<string> captured);
+vector<string> get_adjacent_pieces(Json::Value root, string new_position);
+bool valid_piece(Json::Value root, string position);
 
 int main() {
   // turn movement
@@ -39,11 +43,95 @@ void query_next_move(string player) {
     if (valid_move(player, original_position, new_position)) {
       cout << "Valid Move" << endl;
       make_move(original_position, new_position);
+      vector<string> captured = check_for_capture(new_position);
+      remove_captured(captured);
       valid_move_made = true;
     } else {
       cout << "Invalid Move" << endl;
     }
   }
+}
+
+void remove_captured(vector<string> captured) {
+  std::ifstream ifs("pieces");
+  std::string json_raw( (std::istreambuf_iterator<char>(ifs) ),
+      (std::istreambuf_iterator<char>() ) );
+  Json::Value root;
+  Json::Reader reader;
+  bool parseSuccess = reader.parse(json_raw, root, false);
+
+  for (string position : captured) {
+    root[position] = "none";
+  }
+
+  ofstream file;
+  file.open("pieces");
+  file << root;
+  file.close();
+}
+
+vector<string> check_for_capture(string new_position) {
+  std::map<string, string> opposite;
+  opposite["b"] = "w";
+  opposite["w"] = "b";
+  
+  std::ifstream ifs("pieces");
+  std::string json_raw( (std::istreambuf_iterator<char>(ifs) ),
+      (std::istreambuf_iterator<char>() ) );
+  Json::Value root;
+  Json::Reader reader;
+  bool parseSuccess = reader.parse(json_raw, root, false);
+
+  string self_piece = root[new_position].asString();
+  char ally = self_piece.at(0);
+  vector<string> adjacent_pieces = get_adjacent_pieces(root, new_position);
+  vector<string> ready_for_capture;
+  for (string position : adjacent_pieces) {
+    if (root[position].asString().at(0) != ally) {
+      vector<string> adjacent_pieces_2 = get_adjacent_pieces(root, position);
+      int allies = adjacent_pieces_2.size();
+      if (allies > 1) {
+        ready_for_capture.push_back(position);
+      }
+    }
+  }
+  return ready_for_capture;
+}
+
+bool valid_piece(Json::Value root, string position) {
+  Json::Value node = root[position];
+  if (node.isNull() == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+vector<string> get_adjacent_pieces(Json::Value root, string position) {
+  char first_char = position.at(0);
+  char second_char = position.at(1);
+  vector<string> pieces;
+  if (second_char != 'k') {
+    if (valid_piece(root, first_char+std::to_string((second_char-'0')+1))) {
+      pieces.push_back(first_char+std::to_string((second_char-'0')+1));
+    }
+  }
+  if (second_char != 'a') {
+    if (valid_piece(root, first_char+std::to_string((second_char-'0')-1))) {
+      pieces.push_back(first_char+std::to_string((second_char-'0')-1));
+    }
+  }
+  if (first_char != 11) {
+    if (valid_piece(root, std::string()+(static_cast<char>(first_char+1))+second_char)) {
+      pieces.push_back(std::string()+(first_char++)+second_char);
+    }
+  }
+  if (first_char != 1) {
+    if (valid_piece(root, std::string()+(static_cast<char>(first_char-1))+second_char)) {
+      pieces.push_back(std::string()+(static_cast<char>(first_char-1))+second_char);
+    }
+  }
+  return pieces;
 }
 
 bool valid_move(string player, string original_position, string new_position) {
